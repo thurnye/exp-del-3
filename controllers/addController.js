fea6f0712c2736e882f4bf08e7c8dde374a44254
Daniel Gudi T <danthurnye@gmail.com>
2021-03-02 18:14:54 -0500

    extras

--- controllers/addController.js
+++ controllers/addController.js
@@ -24,7 +24,7 @@ exports.postAddAlbum = (req, res) => {
         tracks: [{
             id: id,
             name: song
-        }];
+        }]
     }
    Albums.albumsArray.push(album);
     res.redirect('/albums');
